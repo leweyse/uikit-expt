bcd14cb5d2b308f8ed8f3e4de613e85f61abdd53
Daryl Castro <darylcastro67@gmail.com>
2025-04-26 12:41:25 +0200

    refactor(vite): enable host

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,14 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  preview: {
+    host: true,
+    port: 3000,
+  },
+  server: {
+    host: true,
+    port: 3000,
+  },
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
