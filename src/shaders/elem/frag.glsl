a9ef04f6bdba62f1274a04599d274157c7c3d7f6
Daryl Castro <darylcastro67@gmail.com>
2025-05-12 22:38:56 +0200

    wip(input): animation

--- src/shaders/elem/frag.glsl
+++ src/shaders/elem/frag.glsl
@@ -1,7 +1,6 @@
 uniform sampler2D uTexture;
 
 varying vec2 vUv;
-varying vec2 vUv1;
 
 void main() {
   vec3 col = texture2D(uTexture, vUv).rgb;
--- src/shaders/elem/vert.glsl
+++ src/shaders/elem/vert.glsl
@@ -2,11 +2,27 @@ uniform float uTime;
 
 varying vec2 vUv;
 
+float PI = 3.1415926535897932384626433832795;
+
+vec4 RotateAroundYInDegrees (vec4 vertex, float degrees) {
+  float alpha = degrees * PI / 180.0;
+  float sina = sin(alpha);
+  float cosa = cos(alpha);
+
+  mat2 m = mat2(cosa, -sina, sina, cosa);
+
+  return vec4((m * vertex.xz), vertex.yw).xzyw;
+}
+
 void main() {
   vUv = uv;
 
+  float progress = cos(uTime * 2.0);
+
+  float angle = progress * 180.0 * (1.0 - uv.x);
+
   vec4 modelPosition = vec4(position, 1.0);
-  modelPosition.z += sin(modelPosition.x * 4.0 + uTime * 2.0) * 0.2;
+  // modelPosition += RotateAroundYInDegrees(modelPosition, angle);
 
   gl_Position = projectionMatrix * modelViewMatrix * modelPosition;
 }
